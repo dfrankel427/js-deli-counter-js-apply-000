4a7d2d9381f0d16cf5264d900790b4b8c5a363b1
Daniel Frankel <dfrankel@uchicago.edu>
2019-02-06 05:39:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function nowServing (katzDeliLine){
 
 function currentLine(katzDeliLine){
    	if (katzDeliLine.length === 0) {return "The line is currently empty."}
- if (katzDeliLine.length > 0) {return (`Currently serving ${firstPersonInLine}.`)
+ if (katzDeliLine.length > 0) {return (`The line is currently ${firstPersonInLine}.`)
 	}
 }
