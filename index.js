ebe3f1059ff7708d979766012c369d03eb59b88b
Daniel Frankel <dfrankel@uchicago.edu>
2019-02-06 04:54:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-var katzDeliLine = []
+var katzDeli = [];
+var katzDeli = []; // At the beginning of the day,
+//the deli is empty and is represented by an empty array
+//<<<<<<<<<--------------- in the instructions <<<<<<
 
-function takeANumber (katzDeli, name) {
-  katzDeliLine.push('${name}');
-  return('Welcome, name. You are number katzDeliLine.length in line.')}
+function takeANumber(katzDeli, name) {
+  katzDeli.push(`${name}`);
+  return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
+}
