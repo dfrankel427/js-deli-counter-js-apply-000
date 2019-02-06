05fc55fb275563bdc1e6e15fc585612e023d4ee9
Daniel Frankel <dfrankel@uchicago.edu>
2019-02-06 05:29:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,8 @@ function takeANumber(katzDeliLine, name) {
   return(`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)}
   
 function nowServing (katzDeliLine){
-  if (katzDeliLine.length === 0) {return "There is nobody waiting to be served!"}
-  if (katzDeliLine.length > 0) {return `Curre
+  	var firstInLine = katzDeliLine.shift()
+  	if (katzDeliLine.length === 0) {return "There is nobody waiting to be served!"}
+  if (katzDeliLine.length > 0) {return 	var firstInLine = katzDeliLine.shift()
+		return "Currently serving " + firstInLine + "."
+	}
