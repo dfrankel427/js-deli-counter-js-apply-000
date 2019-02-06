16f58af61dd37074d03a8398f8db289cd29b6b31
Daniel Frankel <dfrankel@uchicago.edu>
2019-02-06 04:51:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var katzDeliLine = []
 
 function takeANumber (katzDeliLine, name) {
-  katzDeliLine.push('$name');
-  return ()
+  katzDeliLine.push('${name}');
+  return('Welcome, $'{name}. You are number $(katzDeliLine.length))
