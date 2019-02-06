732bc24794d2ba990da95ba79a09b218a8aacad0
Daniel Frankel <dfrankel@uchicago.edu>
2019-02-06 05:41:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ function nowServing (katzDeliLine){
 
 function currentLine(katzDeliLine){
    	if (katzDeliLine.length === 0) {return "The line is currently empty."}
-for(var i=0; i<katzDeliLine.length; i++) {
+var daniel
+for(var i=0; i<katzDeliLine.length; i++) {daniel.push(i+1 + ". "+ katzDeliLine)
   
 }
 	}
